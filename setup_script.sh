42208171bae6c8053f7b545e9401d041677d2a70
Lucas Tyman <lucas-tyman@mths.ca>
2024-02-15 23:20:20 +0000

    initial setup

---
+++ setup_script.sh
@@ -0,0 +1,30 @@
+#!.usr/bin/env bash
+# Created by: Lucas Tyman
+# Created on: Feb 2024
+# AWS Debian Linux .dotfiles and setup script
+
+sudo apt update 
+sudo apt upgrade -y
+
+# common programs
+sudo apt remove w3m -y
+sudo apt install gh -y
+sudo apt install stow -y
+
+# installl NeoVIm from source
+mkdir temp
+cd temp
+sudo apt-get install ninja-build gettext cmake unzip curl
+git clones https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd ..
+cd ..
+sudo rm -r ./temp
+
+# install languages 
+curl -fsSL https://bun.sh/install | bash
+sudo apt install -y default-jdk
+
+# copy over the scripts folder into the home directory
+cp -r ./scripts/ ~/
